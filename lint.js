8f3270b02418f76f5a43aa64aa2ecb8c5abe15ed
Daniel Stuessy <danielstuessy@gmail.com>
2016-07-28 00:02:25 +0100

    Setup JSLint

---
+++ lint.js
@@ -0,0 +1,36 @@
+var fs = require('fs'),
+	LintStream = require('jslint').LintStream;
+
+var filename = 'src/animate.js';
+var lintStream = new LintStream({
+	edition: 'latest',
+	length: 100,
+	single: true,
+	browser: true,
+	predef: ['window', 'console']
+});
+
+fs.readFile(filename, function(err, data) {
+
+	console.log(`JSLint: Linting ${filename}`);
+
+	lintStream.write({
+		file: filename,
+		body: data.toString()
+	});
+
+	lintStream.on('data', function (chunk) {
+		
+	    console.log(`JSLint: ${chunk.linted.errors.length} errors found:\n`);
+
+	    chunk.linted.errors.forEach((lintError) => {
+	    	var line = lintError.line,
+	    		col = lintError.column,
+	    		message = lintError.message;
+
+	    	console.log(`line: ${line}, column: ${col}`);
+	    	console.log(`${message}`);
+	    	console.log('');
+	    });
+	});
+});
